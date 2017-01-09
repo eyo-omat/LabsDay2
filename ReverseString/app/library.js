74f1bfe0a39471a25f8dfb19a4c1cbea97790853
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-09 20:43:10 +0100

    add reverseString function to library.js

--- ReverseString/app/library.js
+++ ReverseString/app/library.js
@@ -3,6 +3,16 @@
 module.exports = {
 
 reverseString: function(str){
+    if (!str) { return null; }
+    var revStr = '';
+    for (var i = str.length - 1; i >= 0; i--){
+        revStr += str[i];
+    }
         
+    if (str === revStr) {
+        return true;
+    } else {
+        return revStr;
+    }
 }
 }
