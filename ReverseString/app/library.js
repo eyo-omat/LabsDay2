5570b35218b7f7b937163413746c5210e93c5493
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-09 21:02:48 +0100

    replace for loop with while loop in reverseString function

--- ReverseString/app/library.js
+++ ReverseString/app/library.js
@@ -5,10 +5,11 @@ module.exports = {
 reverseString: function(str){
     if (!str) { return null; }
     var revStr = '';
-    for (var i = str.length - 1; i >= 0; i--){
+    var i = str.length - 1;
+    /*for (var i = str.length - 1; i >= 0; i--){
         revStr += str[i];
-    }
-        
+    }*/
+    while(i--) { revStr += str[i];   } 
     if (str === revStr) {
         return true;
     } else {
