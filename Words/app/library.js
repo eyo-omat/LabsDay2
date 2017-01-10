f26d0741363baff977530ce95dfe35aabbfc0f43
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-10 14:55:29 +0100

    clean up library.js for words project

--- Words/app/library.js
+++ Words/app/library.js
@@ -3,46 +3,21 @@
 module.exports = {
 
 words: function(str){
-       /* var wordCount = str.split(" ").length;
-        var wordsArray = str.split(" ");
-        var obj = new Object();
-        for (var i = 0; i < a.length; i++) {
-            obj[a[i]] = a[i];
-        }
-        var countArray = [];
-        var count = 0;
-        console.log(wordCount);
-        console.log(wordsArray);
-        for (var i = 0; i < wordCount; i++) {
-            var pos = 0;
-            if (wordsArray[pos] === wordsArray[i]) { countArray[0] = 1+count; }
-                pos++; 
-                console.log(pos);
-        }*/
+
 var word = str.replace(/\s\s*/g, ' ');
 var word = word.replace(/[\r\n]\s*/g, ' ');
-var wordsplit = word.split(" ");
-var obj = { };
-for (var i = 0, j = wordsplit.length; i < j; i++) {
-  if (obj[wordsplit[i]]) {
-     obj[wordsplit[i]]++;
+var wordsArray = word.split(" ");
+var wordCount = wordsArray.length;
+var wordFrequecies = { };
+for (var i = 0, j = wordCount; i < j; i++) {
+  if (wordFrequecies[wordsArray[i]]) {
+     wordFrequecies[wordsArray[i]]++;
   }
   else {
-     obj[wordsplit[i]] = 1;
+     wordFrequecies[wordsArray[i]] = 1;
   }
 }
 
-return obj;
-/*var fruits = str;
-var a = fruits.split(" ");
-var obj = new Object();
-for (var i = 0; i < a.length; i++) {
-    obj[a[i]] = a[i];
-}
-for (var key in obj) {
-   console.log(key);
-   fruits = str.split(key);
-}
-   console.log(fruits); */   
+return wordFrequecies;  
 }
 }
