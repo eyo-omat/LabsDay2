cb1ae72d6f26501a32bbd34982d9cd22adb8c918
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-11 01:00:18 +0100

    add tweetbot to repository

---
+++ tweetbot/config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'rX37S6vVLbP2hM8kUWrr4nnr1',  
+  consumer_secret: 'wXzxQUdNEGven4KrwwNgsw9DmmMpxWQUjlpXsFGOYx1aQDVCzI',
+  access_token: '65937237-mWyJ69WRHADS0r36HfwkNoL8FRH0Sqvsphwuia5T7',  
+  access_token_secret: '6AAEnBLeUJmK5cbKZEwtrsOBbOhYPeCznfVL32IOUIuK9'
+}
